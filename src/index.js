c444b0fb2d88ea5ebbc87008491c9cca365af43a
NielzosFilms <nielzosfilms@gmail.com>
2020-09-23 20:22:33 +0200

    graphql setup

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import GraphiQL from "graphiql";
+import {
+	ApolloProvider,
+	ApolloClient,
+	InMemoryCache,
+	gql,
+	NormalizedCacheObject,
+} from "@apollo/client";
+import typeDefs from "./server/schema";
+
+const client = new ApolloClient({
+	uri: "http://localhost:3000/graphql",
+	cache: new InMemoryCache(),
+	typeDefs,
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<ApolloProvider client={client}>
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	</ApolloProvider>,
+	document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
---
+++ src/server/schema.js
@@ -0,0 +1,31 @@
+const {gql} = require("@apollo/client");
+
+const typeDefs = gql`
+	type User {
+		id: ID!
+		username: String!
+		password: String!
+		color: String
+	}
+
+	type Boodschap {
+		id: ID!
+		name: String!
+		amount: Int
+		checked: Boolean!
+	}
+
+	type Query {
+		users: [User]!
+		user(id: ID!): User
+
+		boodschappen: [Boodschap]!
+		boodschap(id: ID!): Boodschap
+	}
+
+	#type Mutation {
+	#
+	#}
+`;
+
+module.exports = typeDefs;

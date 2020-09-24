d21ce44be3e9fd46e6377f78cc9a9a5c1c9c082e
NielzosFilms <nielzosfilms@gmail.com>
2020-09-24 21:42:23 +0200

    fixed groceries nullable

--- src/components/groceries/GroceryForm.js
+++ src/components/groceries/GroceryForm.js
@@ -1,85 +1,85 @@
 import React from "react";
-import { gql, useMutation } from "@apollo/client";
+import {gql, useMutation} from "@apollo/client";
 
 const ADD_GROCERY = gql`
-    mutation CreateGrocery($name: String!, $amount: Int) {
-        createGrocery(name: $name, amount: $amount) {
-            id
-            name
-            checked
-        }
-    }
+	mutation CreateGrocery($name: String!, $amount: Int) {
+		createGrocery(name: $name, amount: $amount) {
+			id
+			name
+			checked
+		}
+	}
 `;
 
 export default function GroceryForm(props) {
-    const [addGrocery] = useMutation(ADD_GROCERY);
+	const [addGrocery] = useMutation(ADD_GROCERY);
 
-    const [amount, setAmount] = React.useState();
-    const [name, setName] = React.useState();
+	const [amount, setAmount] = React.useState();
+	const [name, setName] = React.useState();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        if (name == null) {
-            props.createAlert("danger", "Geen product naam gegeven.");
-            return;
-        }
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		if (name == null || name == "") {
+			props.createAlert("danger", "Geen product naam gegeven.");
+			return;
+		}
 
-        addGrocery({ variables: { name, amount: parseInt(amount) } })
-            .then((result) => {
-                setAmount("");
-                setName("");
-                props.refetch();
-            })
-            .catch((error) => {
-                console.log(error);
-                props.createAlert("danger", "Er is iets fout gegaan.");
-            });
-    };
+		addGrocery({variables: {name, amount: parseInt(amount)}})
+			.then((result) => {
+				setAmount("");
+				setName("");
+				props.refetch();
+			})
+			.catch((error) => {
+				console.log(error);
+				props.createAlert("danger", "Er is iets fout gegaan.");
+			});
+	};
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <h4>Item toevoegen</h4>
-            <div className="form-group">
-                <div className="form-row">
-                    <div className="col-2">
-                        <input
-                            type="number"
-                            min="1"
-                            max="99"
-                            placeholder="Aantal"
-                            className="form-control"
-                            value={amount}
-                            onChange={(e) => setAmount(e.target.value)}
-                        />
-                    </div>
-                    <div className="col-9">
-                        <input
-                            type="text"
-                            className="form-control"
-                            placeholder="Product"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                        />
-                    </div>
-                    <div className="col-1">
-                        <button type="submit" className="btn btn-primary w-100">
-                            <svg
-                                width="1.5em"
-                                height="1.5em"
-                                viewBox="0 0 16 16"
-                                className="bi bi-plus"
-                                fill="currentColor"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                    fillRule="evenodd"
-                                    d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"
-                                />
-                            </svg>
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </form>
-    );
+	return (
+		<form onSubmit={handleSubmit}>
+			<h4>Item toevoegen</h4>
+			<div className="form-group">
+				<div className="form-row">
+					<div className="col-2">
+						<input
+							type="number"
+							min="1"
+							max="99"
+							placeholder="Aantal"
+							className="form-control"
+							value={amount}
+							onChange={(e) => setAmount(e.target.value)}
+						/>
+					</div>
+					<div className="col-9">
+						<input
+							type="text"
+							className="form-control"
+							placeholder="Product"
+							value={name}
+							onChange={(e) => setName(e.target.value)}
+						/>
+					</div>
+					<div className="col-1">
+						<button type="submit" className="btn btn-primary w-100">
+							<svg
+								width="1.5em"
+								height="1.5em"
+								viewBox="0 0 16 16"
+								className="bi bi-plus"
+								fill="currentColor"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path
+									fillRule="evenodd"
+									d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"
+								/>
+							</svg>
+						</button>
+					</div>
+				</div>
+			</div>
+		</form>
+	);
 }
--- src/server/db/index.js
+++ src/server/db/index.js
@@ -1,26 +1,26 @@
-const { Sequelize, DataTypes, Model } = require("sequelize");
+const {Sequelize, DataTypes, Model} = require("sequelize");
 
-const sequelize = new Sequelize("family_app", "niels", "", {
-    host: "localhost",
-    dialect: "mysql",
+const sequelize = new Sequelize("family_app", "root", "", {
+	host: "localhost",
+	dialect: "mysql",
 });
 module.exports.sequelize = sequelize;
 
 async function connect() {
-    try {
-        await sequelize.authenticate();
-        console.log("Connection has been established successfully.");
-    } catch (error) {
-        console.error("Unable to connect to the database:", error);
-    }
+	try {
+		await sequelize.authenticate();
+		console.log("Connection has been established successfully.");
+	} catch (error) {
+		console.error("Unable to connect to the database:", error);
+	}
 }
 connect();
 
 // migrate database
 /*
 async function syncDatabase() {
-    await sequelize.sync({ force: false, alter: true });
-    console.log("Synced");
+	await sequelize.sync({force: false, alter: true});
+	console.log("Synced");
 }
 syncDatabase();
 */

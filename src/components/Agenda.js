cb01c99023d5f7eed14c6b4c6761c345fc123097
niels.hazelaar <niels.hazelaar@feka.nl>
2020-09-28 13:49:09 +0200

    changes

--- src/components/Agenda.js
+++ src/components/Agenda.js
@@ -1,43 +1,31 @@
 import React from "react";
 import AgendaTable from "./AgendaTable";
-import {getItems} from "../queries/agendaItem";
 
 function Agenda() {
-	const [items, setItems] = React.useState();
-	const date = new Date();
+    const date = new Date();
 
-	/*console.log(date.getDate()); //day number
+    /*console.log(date.getDate()); //day number
     console.log(date.getMonth() + 1); //Month
     console.log(date.getFullYear());
 
     console.log(date.getHours());
     console.log(date.getMinutes());*/
 
-	React.useEffect(() => {
-		async function fetchItems() {
-			const result = await getItems();
-			setItems(result);
-		}
-		if (!items) {
-			fetchItems();
-		}
-	});
-
-	if (items) {
-		return (
-			<div className="p-3">
-				<h2>Agenda</h2>
-				<div className="dropdown-divider"></div>
-				<AgendaTable items={items} />
-			</div>
-		);
-	} else {
-		return (
-			<div className="p-3">
-				<h2>Agenda</h2>
-				<div className="dropdown-divider"></div>
-				<p>Nothing here yet :)</p>
-				{/* <center>
+    if (false) {
+        return (
+            <div className="p-3">
+                <h2>Agenda</h2>
+                <div className="dropdown-divider"></div>
+                <AgendaTable items={null} />
+            </div>
+        );
+    } else {
+        return (
+            <div className="p-3">
+                <h2>Agenda</h2>
+                <div className="dropdown-divider"></div>
+                <p>Nothing here yet :)</p>
+                {/* <center>
 					<div
 						className="spinner-border text-secondary"
 						role="status"
@@ -45,9 +33,9 @@ function Agenda() {
 						<span className="sr-only">Loading...</span>
 					</div>
 				</center> */}
-			</div>
-		);
-	}
+            </div>
+        );
+    }
 }
 
 export default Agenda;

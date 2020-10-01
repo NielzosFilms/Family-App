f8b2824628c68410bf79806b889240987c942ae9
NielzosFilms <nielzosfilms@gmail.com>
2020-10-01 17:50:14 +0200

    database connection changes

--- src/server/db/index.js
+++ src/server/db/index.js
@@ -1,32 +1,32 @@
-const { Sequelize } = require("sequelize");
+const {Sequelize} = require("sequelize");
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    {
-        host: process.env.REACT_APP_DEV_DB_HOST,
-        dialect: "mysql",
-    }
+	process.env.DB_NAME,
+	process.env.DB_USER,
+	process.env.DB_PASS,
+	{
+		host: process.env.DB_HOST,
+		dialect: "mysql",
+	}
 );
 module.exports.sequelize = sequelize;
 
 async function connect() {
-    try {
-        await sequelize.authenticate();
-        console.log("Connection has been established successfully.");
-    } catch (error) {
-        console.error("Unable to connect to the database:", error);
-    }
+	try {
+		await sequelize.authenticate();
+		console.log("Connection has been established successfully.");
+	} catch (error) {
+		console.error("Unable to connect to the database:", error);
+	}
 }
 connect();
 
 async function syncDatabase() {
-    await sequelize.sync({ force: false, alter: true });
-    console.log("Synced");
-    process.exit();
+	await sequelize.sync({force: false, alter: true});
+	console.log("Synced");
+	process.exit();
 }
 
 if (process.argv[2] == "migrate") {
-    syncDatabase();
+	syncDatabase();
 }
